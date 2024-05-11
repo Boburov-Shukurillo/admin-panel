e4a66b87caaf2ff1f25a801bd73cc4d6c59d3d5a
Boburov-Shukurillo <boburovshukurilo@gmail.com>
2024-05-11 13:12:54 +0500

    responive

--- src/pages/AdminPanel.jsx
+++ src/pages/AdminPanel.jsx
@@ -4,8 +4,8 @@ const AdminPanel = ({ isLogin }) => {
   return (
     <div>
       {
-        <div className='max-w-7xl mx-auto flex items-center justify-center py-20'>
-          <form action="" className='flex flex-col items-center justify-between p-20 w-1/2 gap-5 drop-shadow-lg shadow-md rounded-xl'>
+        <div className='max-w-7xl mx-auto flex items-center justify-center py-20 max-sm:p-5 max-sm:pt-20'>
+          <form action="" className='flex flex-col items-center justify-between p-20 w-1/2 max-sm:w-full  max-sm:p-3 gap-5 drop-shadow-lg shadow-md rounded-xl'>
             <h1 className='text-4xl font-mono font-bold'>Husan Art</h1>
             <input type="text" className={`border-2 rounded-lg px-6 py-3 w-full focus:border-blue-500 outline-none`}
               name="" id="" placeholder='Enter image name' />
